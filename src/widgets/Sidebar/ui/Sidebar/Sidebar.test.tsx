7c9d45e9892fd154508bfd9ccaac28cea7796e97
aapanasov <aapanasov@gmail.com>
2023-02-18 15:20:58 +0300

    fix: fix sidebar unit tests

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,17 +1,17 @@
 import { fireEvent, screen } from '@testing-library/react';
 import {
-  renderWithTranslation,
-} from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+  componentRender,
+} from 'shared/lib/tests/componentRender';
 import { Sidebar } from './Sidebar';
 
 describe('Sidebar', () => {
   test('render sidebar', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
   });
 
   test('sidebar toggle', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     const toggleBtn = screen.getByTestId('sidebar-toggle');
     const sideBar = screen.getByTestId('sidebar');
     expect(sideBar).toBeInTheDocument();
