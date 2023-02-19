8514720dfbfc1c26e5a82a0537d5ecfab280e413
aapanasov <aapanasov@gmail.com>
2023-02-19 17:16:37 +0300

    fix: disable build analyzer for cli

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,7 +1,7 @@
 import HTMLWebpackPlugin from 'html-webpack-plugin';
 import { ProgressPlugin, WebpackPluginInstance, DefinePlugin } from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+// import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
 export default function buildPlugins({ paths, isDev }: BuildOptions): WebpackPluginInstance[] {
@@ -17,9 +17,10 @@ export default function buildPlugins({ paths, isDev }: BuildOptions): WebpackPlu
     new DefinePlugin({ IS_DEV: JSON.stringify(isDev) }),
   ];
 
-  if (!isDev) {
-    plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }));
-  }
+  // TODO: fix analyzer
+  // if (!isDev) {
+  //   plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }));
+  // }
 
   return plugins;
 }
